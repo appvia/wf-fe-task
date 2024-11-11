d27c9ab0023d350874a51f7027a6618cc406eff6
Anthony Guido <guido.anthony@orange.fr>
2024-11-11 18:38:30 +0100

    fix: build

--- src/components/Wizard/components/WizardSteps/FeatureSelectList/FeatureSelectList.constant.ts
+++ src/components/Wizard/components/WizardSteps/FeatureSelectList/FeatureSelectList.constant.ts
@@ -1,7 +1,7 @@
 import { Feature } from "./FeatureSelectList.types";
 
 export const FEATURE_OPTIONS: Feature[] = [
-  { id: 1, name: "Feature A", description: "Description for Feature A" },
-  { id: 2, name: "Feature B", description: "Description for Feature B" },
-  { id: 3, name: "Feature C", description: "Description for Feature C" },
+  { id: "1", name: "Feature A", description: "Description for Feature A" },
+  { id: "2", name: "Feature B", description: "Description for Feature B" },
+  { id: "3", name: "Feature C", description: "Description for Feature C" },
 ];
